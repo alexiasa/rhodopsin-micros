1606ed70d194a44ba4f3d27048817347f954e916
alexiasa <alexia.salone@icloud.com>
2019-02-28 22:47:25 -0800

    expanded GetIps controller to handle a query string of 'addr' to support returning single IP details.  it should no longer be necessary to implement separate routes for this functionality.

--- ips/controllers/controller.go
+++ ips/controllers/controller.go
@@ -11,25 +11,42 @@ func GetIps(w http.ResponseWriter, r *http.Request) {
 	defer context.Close()
 	col := context.DbCollection("ips")
 	repo := &data.IpRepository{C: col}
-	ips := repo.GetAll()
-	j, err := json.Marshal(ips)
-	if err != nil {
-		http.Error(w, "oh noes! something bad happened", http.StatusInternalServerError)
-	}
 
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(j)
+	params, ok := r.URL.Query()["addr"]
+
+	if !ok || (len(params[0]) < 1) {
+		// this branch should happen if there isn't a query string value - null bool = true or params is nil
+		ips := repo.GetAll()
+		j, err := json.Marshal(ips)
+		if err != nil {
+			http.Error(w, "oh noes! something bad happened", http.StatusInternalServerError)
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(j)
+	} else {
+		// this branch should happen if there is a query string value ?Addr=something in params
+		ip, err := repo.GetByIpaddr(params[0])
+		j, err := json.Marshal(ip)
+		if err != nil {
+			http.Error(w, "oh noes! something bad happened", http.StatusInternalServerError)
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(j)
+
+	}
 
 }
 
-// todo: implement controller to return single IP address - line 32
+// todo: implement controller to return details for single IP found by address - line 32
 /*func GetIpAddr(ipaddr string) (w http.ResponseWriter, r *http.Request) {
 	context := NewContext()
 	defer context.Close()
 	col := context.DbCollection("ips")
 	repo := &data.IpRepository{C: col}
-	ip := repo.GetByIpaddr(ipaddr)
+	ip, err := repo.GetByIpaddr(ipaddr)
 	j, err := json.Marshal(ip)
 	if err != nil {
 		http.Error(w, "oh noes! something bad happened", http.StatusInternalServerError)
--- ips/routers/ips.go
+++ ips/routers/ips.go
@@ -7,10 +7,9 @@ import (
 
 // SetIpRoutes configures routes for ip entity
 func SetIpRoutes(router *mux.Router) *mux.Router {
-	//ipRouter := mux.NewRouter()
 	router.HandleFunc("/ips", controllers.GetIps).Methods("GET")
 	// todo: implement route for getting IPs by ipaddr value
-	// ipRouter.HandleFunc("/ips/{id}", controllers.GetIpAddr).Methods("GET")
+	//router.HandleFunc("/ips/{id}", controllers.GetIpAddr()).Methods("GET")
 	// todo: implement route for getting IPs by database ID value
 	// ipRouter.HandleFunc("/ips/{id}", controllers.GetById).Methods("GET")
 
